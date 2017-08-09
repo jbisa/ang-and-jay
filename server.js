13dd18f9850d988b9f9a3e3ad325088f850cb7a7
Jay Bisa <jay.bisa@gmail.com>
2017-08-08 22:56:51 -0400

    Modify server js.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'src/index.html'));
   res.sendFile(path.resolve(__dirname, 'src/client.min.js'));
   res.sendFile(path.resolve(__dirname, 'src/css/styles.css'));
