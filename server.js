d78582e44fae7499b00885a1910759f2b14c9c86
Jay Bisa <jay.bisa@gmail.com>
2017-08-13 17:36:40 -0400

    Change server.js file.

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
-const app = express();
+const express = require('express')
+const path = require('path')
+const port = process.env.PORT || 8080
+const app = express()
+// this assumes that all your app files
+// `src` directory relative to where your server.js is
+app.use(express.static(__dirname + '/src'))
 
-app.use(express.static(__dirname));
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'src', 'index.html'))
+})
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'src/index.html'));
-});
-
-app.listen(port);
-console.log('Server started');
+app.listen(port)
+console.log("Server started on port " + port)
