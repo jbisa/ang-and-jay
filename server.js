6be34b2ac5485cfdcf3d945b54e373244d4ab01b
Jay Bisa <jay.bisa@gmail.com>
2017-08-17 21:16:08 -0400

    Remove redis.

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
 const express = require('express')
 const path = require('path')
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const redis = require('redis')
+//const bodyParser = require('body-parser')
+//const methodOverride = require('method-override')
+//const redis = require('redis')
 const port = process.env.PORT || 8080
 
 // init app
 const app = express();
 
 // create Redis client
+/*
 let client = redis.createClient();
 client.on('connect', function() {
   console.log('connected to redis...');
-});
+});*/
 
 // this assumes that all your app files
 // `src` directory relative to where your server.js is
 app.use(express.static(__dirname + '/src'));
 
 // methodOverride
-app.use(methodOverride('_method'));
+//app.use(methodOverride('_method'));
 
 // body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('*', function (request, response) {
   response.sendFile(path.resolve(__dirname, 'src', 'index.html'));
 });
 
 // enter the Site
+/*
 app.post('/#/enter', function (request, response) {
   console.log(request.body.code);
   //response.sendFile(path.resolve(__dirname, 'src', 'index.html'));
-});
+});*/
 
 app.listen(port)
 console.log("Server started on port " + port)
