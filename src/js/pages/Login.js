65d2f10cb83b3480523a8713a3ad7859c27826df
Jay Bisa <jay.bisa@gmail.com>
2017-08-19 13:30:21 -0400

    Add some error messaging.

--- src/js/pages/Login.js
+++ src/js/pages/Login.js
@@ -4,6 +4,10 @@ import Footer from "../components/Footer";
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showPasscodeError: false
+    }
+    // Check to see if the user already entered the correct passcode
     if (window.sessionStorage.getItem("codeEntered") === "528926") {
       this.props.history.push("/home");
     }
@@ -11,14 +15,34 @@ export default class Login extends React.Component {
 
   enterSite(e) {
     e.preventDefault();
+    this.checkPasscode();
+  }
+
+  handleKeyUp(e) {
+    e.preventDefault();
+    // Enter key
+    if (e.keyCode === 13) {
+      this.checkPasscode();
+    }
+  }
+
+  checkPasscode() {
     if (this.refs.code !== null) {
     	var input = this.refs.code;
 			var inputValue = input.value;
       window.sessionStorage.setItem("codeEntered", inputValue);
       if (inputValue === "528926") {
         this.props.history.push("/home");
+      } else {
+        this.showPasscodeError(true);
       }
+    } else {
+      this.showPasscodeError(true);
     }
+  }
+
+  showPasscodeError(showPasscodeError) {
+    this.setState({showPasscodeError});
   }
 
   render() {
@@ -33,9 +57,10 @@ export default class Login extends React.Component {
               <h2 class="home-h2">We're getting married!</h2>
               <p>Please enter the six digit code you received to enter the site.</p>
               <div>
-                <input class="input-field" type="text" ref="code" placeholder="Site Code" />
-                <button class="button" type="submit" onClick={this.enterSite.bind(this)}>Enter Site</button>
+                <input class="input-field" type="text" ref="code" placeholder="Site Code" onKeyUp={this.handleKeyUp.bind(this)}/>
+                <button id="enterButton" class="button" type="submit" onClick={this.enterSite.bind(this)}>Enter Site</button>
               </div>
+              {this.state.showPasscodeError && <p class="error-msg">Sorry, the passcode you entered is incorrect.</p>}
             </div>
           </div>
         </div>
